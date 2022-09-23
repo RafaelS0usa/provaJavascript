c11c7678229b87f3e62b37de4e79a8920a1cc376
Rafael Sousa <sousarafael151@gmail.com>
2022-09-23 20:58:17 -0300

    Prova Javascript

---
+++ provaJS.js
@@ -0,0 +1,14 @@
+var bi1 = 8
+var bi2 = 9
+var bi3 = 6
+var bi4 = 7.5
+var media = 7
+
+var mediaBimestral = (bi1+bi2+bi3+bi4)
+var mediaAnual = mediaBimestral/4
+
+if(mediaAnual >= media){
+    console.log("Parabéns você foi Aprovado")
+}else{
+    console.log("Data da Recuperação: 26/12/2022")
+}
